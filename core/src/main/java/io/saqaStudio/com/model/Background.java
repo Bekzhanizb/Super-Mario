1aac55cb197e150b03ca18ff042c0ca7a07b67d0
Bekezhan <bekzhanizbasar228@gmail.com>
2025-05-15 02:50:56 +0500

    Fixed errors

--- core/src/main/java/io/saqaStudio/com/model/Background.java
+++ core/src/main/java/io/saqaStudio/com/model/Background.java
@@ -24,50 +24,46 @@ public class Background {
 
     private void initLevel(int level) {
         if (level == 1) {
-            for (int i = 0; i < 15; i++) {
-                obstructions.add(new Enemy(i * 60, 540, 9));
+            // Земля (нижний ряд из блоков)
+            for (int i = 0; i < 16; i++) {
+                obstructions.add(new Enemy(i * 60, 0, 9));
             }
-            obstructions.add(new Enemy(120, 360, 4));
-            obstructions.add(new Enemy(300, 360, 0));
-            obstructions.add(new Enemy(360, 360, 4));
-            obstructions.add(new Enemy(420, 360, 0));
-            obstructions.add(new Enemy(480, 360, 4));
-            obstructions.add(new Enemy(540, 360, 0));
-            obstructions.add(new Enemy(420, 180, 0));
 
-            enemies.add(new MoveEnemy(690, 360, true, 2, 420, 540, this));
+            // Платформы ниже
+            obstructions.add(new Enemy(60, 180, 4));
+            obstructions.add(new Enemy(180, 180, 0));
+            obstructions.add(new Enemy(240, 180, 4));
+            obstructions.add(new Enemy(360, 60, 0));
 
-            obstructions.add(new Enemy(660, 540, 6));
-            obstructions.add(new Enemy(720, 540, 5));
-            obstructions.add(new Enemy(660, 480, 8));
-            obstructions.add(new Enemy(720, 480, 7));
-
-            enemies.add(new MoveEnemy(300, 200, true, 1, this));
+            // Враги ближе к игроку и чуть уменьшены по Y
+            enemies.add(new MoveEnemy(600, 60, true, 1, this));
+            enemies.add(new MoveEnemy(450, 60, true, 2, 60, 180, this));
+            enemies.add(new MoveEnemy(300, 60, true, 1, this));
         }
 
         if (level == 2) {
             for (int i = 0; i < 15; i++) {
-                if (i != 10) obstructions.add(new Enemy(i * 60, 540, 9));
+                if (i != 10) obstructions.add(new Enemy(i * 60, 0, 9));
             }
-            obstructions.add(new Enemy(60, 540, 6));
-            obstructions.add(new Enemy(120, 540, 5));
-            obstructions.add(new Enemy(60, 480, 6));
-            obstructions.add(new Enemy(120, 480, 5));
-            obstructions.add(new Enemy(60, 420, 8));
-            obstructions.add(new Enemy(120, 420, 7));
+            obstructions.add(new Enemy(60, 0, 6));
+            obstructions.add(new Enemy(120, 0, 5));
+            obstructions.add(new Enemy(60, 60, 6));
+            obstructions.add(new Enemy(120, 60, 5));
+            obstructions.add(new Enemy(60, 120, 8));
+            obstructions.add(new Enemy(120, 120, 7));
 
-            enemies.add(new MoveEnemy(90, 540, true, 2, 350, 540, this));
-            enemies.add(new MoveEnemy(270, 540, true, 2, 300, 400, this));
+            enemies.add(new MoveEnemy(90, 60, true, 2, 60, 180, this));
+            enemies.add(new MoveEnemy(270, 60, true, 2, 60, 180, this));
 
-            turtle = new Turtle(680, 480);
+            turtle = new Turtle(680, 60);
         }
 
         if (level == 3) {
             for (int i = 0; i < 15; i++) {
-                obstructions.add(new Enemy(i * 60, 540, 9));
+                obstructions.add(new Enemy(i * 60, 0, 9));
             }
             obstructions.add(new Enemy(550, 180, 11));
-            obstructions.add(new Enemy(520, 480, 2));
+            obstructions.add(new Enemy(520, 300, 2));
         }
     }
 
